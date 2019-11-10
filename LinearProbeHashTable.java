8f6811c3eeb7ef9a1c98042af2222acdc2ef9b5f
Arijeet Choudhury <40137186+arijeetchoudhury100@users.noreply.github.com>
2019-11-10 23:06:49 +0530

    Update LinearProbeHashTable.java

--- LinearProbeHashTable.java
+++ LinearProbeHashTable.java
@@ -1,12 +1,9 @@
-public class LinearProbeHashTable{
-    int tableSize;
-    int curSize;
-    int[] hashTable;
+package assignment3.collision;
 
+public class LinearProbeHashTable extends HashTable{
+  
     LinearProbeHashTable(int tableSize){
-        this.tableSize = findNextPrime(tableSize);
-        hashTable = new int[this.tableSize];
-        curSize = 0;
+        super(tableSize);
     }
 
     public int insert(int n){
@@ -24,41 +21,6 @@ public class LinearProbeHashTable{
         hashTable[currentPos] = n;
         curSize++;
         return collisions;
-    }
-
-    public void print(){
-        for(int i=0;i<this.tableSize;i++){
-            System.out.println(i+":"+hashTable[i]);
-        }
-    }
-
-    public int getCurrentSize(){
-        return curSize;
-    }
-
-    public int getTableSize(){
-        return tableSize;
-    }
-    private int hash(int n){
-        return n % this.tableSize;
-    }
-
-    private int findNextPrime(int n){
-        while(true){
-            boolean prime = true;
-            if(n%2 == 0)
-                n++;
-            for(int i=3;i<=Math.floor(Math.sqrt(n));i+=2){
-                if(n%i == 0){
-                    prime = false;
-                    break;
-                }
-            }
-            if(prime == false)
-                n = n+2;
-            else
-                return n;
-        }
     }
 }
 
